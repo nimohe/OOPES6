b3cbd510a083356636c6e62c5d824ad2650ffb7e
mreza.rajabi <mreza.rajabi@gmail.com>
2017-07-31 01:13:53 +0200

    chapter 6-part 1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-import { Car } from './classes/car.js';
-import { Drone } from './classes/drone.js';
-import { fleet } from './fleet-data.js';
-import { FleetDataService } from './services/fleet-data-service.js';
+import $ from 'jquery';
+import {Car} from './classes/car.js';
+import {Drone} from './classes/drone.js';
+import {fleet} from './fleet-data.js';
+import {FleetDataService} from './services/fleet-data-service.js';
+import {Button} from './ui/button.js';
+import {Image} from './ui/image.js';
+import {TitleBar} from './ui/title-bar.js';
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-
-let car = dataService.getCarByLicense('AT9900');
-
-// let cars = dataService.getCarsSortedByLicense();
-
-// let cars = dataService.filterCarsByMake('e');
-
-// for (let car of cars)
-//     console.log(car.make);
+let tb = new TitleBar('Our Application');
+tb.addLink('Home', '');
+tb.addLink('Cars', '');
+tb.addLink('Drones', '');
+tb.addLink('Map', '');
+tb.appendToElement($('body'));
